e828fcc26fc5de1be08b2d96aedcc67a0116d5fc
Brett Bash <brettbash@brettbash.com>
2024-02-19 07:52:06 -0600

    Harpoon Updates
    Using Grapple instead of Harpoon for more control

--- vim/lua/plugins/harpoon.lua
+++ vim/lua/plugins/harpoon.lua
@@ -1,47 +1,84 @@
+-- 󰆧 Lament Configuration 󰛢 󰯈  --
+-- We have such sights to show you --
+-- 󰫣 --
 return {
-  "theprimeagen/harpoon",
+  "cbochs/grapple.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
   name = "harpoon",
+
+  config = function()
+    require("lualine").setup({
+      sections = {
+        lualine_b = {
+          {
+            function()
+              local key = require("grapple").key()
+              return "󰛢---->> [󰯈 " .. key .. "]"
+            end,
+            cond = require("grapple").exists,
+          },
+        },
+      },
+    })
+    require("telescope").load_extension("grapple")
+    -- vim.keymap.set("n", "<leader>j", function()
+    --   require("grapple").select({ key = "{name}" })
+    -- end, { desc = "Harpoon Select" })
+  end,
+
   keys = {
     {
-      "<leader>l",
-      "<cmd>lua require('harpoon.mark').add_file()<cr>",
-      desc = "Harpoon Add File",
+      "<leader>m",
+      "<cmd>lua require('grapple').toggle()<cr>",
+      desc = "Harpoon Hook",
     },
 
     {
       "<C-r>",
-      "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
+      "<cmd>lua require('grapple').popup_tags()<cr>",
       desc = "Harpoon Menu",
-    },
-
-    {
-      "<C-n>",
-      "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",
-      desc = "Harpoon 1",
-    },
-
-    {
-      "<C-e>",
-      "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",
-      desc = "Harpoon 2",
-    },
-
-    {
-      "<C-i>",
-      "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",
-      desc = "Harpoon 3",
-    },
-
-    {
-      "<C-o>",
-      "<cmd>lua require('harpoon').ui.nav_file(4)<cr>",
-      desc = "Harpoon 4",
     },
 
     {
       "<leader>fh",
-      "<cmd>Telescope harpoon marks<cr>",
-      desc = "Harpoon Menu",
+      "<cmd>Telescope grapple tags<cr>",
+      desc = "Harpoon Telescope Menu",
+    },
+
+    {
+      "<leader>j1",
+      "<cmd>lua require('grapple').select({ key = 1 })<cr>",
+      desc = "Harpoon Select 1",
+    },
+
+    {
+      "<leader>j2",
+      "<cmd>lua require('grapple').select({ key = 2 })<cr>",
+      desc = "Harpoon Select 2",
+    },
+
+    {
+      "<leader>j3",
+      "<cmd>lua require('grapple').select({ key = 3 })<cr>",
+      desc = "Harpoon Select 3",
+    },
+
+    {
+      "<leader>j4",
+      "<cmd>lua require('grapple').select({ key = 4 })<cr>",
+      desc = "Harpoon Select 4",
+    },
+
+    {
+      "<leader>j5",
+      "<cmd>lua require('grapple').select({ key = 5 })<cr>",
+      desc = "Harpoon Select 5",
+    },
+
+    {
+      "<leader>j6",
+      "<cmd>lua require('grapple').select({ key = 6 })<cr>",
+      desc = "Harpoon Select 6",
     },
   },
 }
