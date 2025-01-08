e4d3938a26e1e01dbc7b4393889a341d528d9138
Brett Bash <brettbash@brettbash.com>
2025-01-08 06:07:37 -0600

    Blink Rollback until Snippet Fix is Pushed

--- vim/lua/plugins/luasnip.lua
+++ vim/lua/plugins/luasnip.lua
@@ -1,6 +1,8 @@
 return {
   "L3MON4D3/LuaSnip",
+  version = "v2.*",
   config = function()
     require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
   end,
+  build = "make install_jsregexp",
 }
