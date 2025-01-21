54c96e1b6ed2e35b3f9cc546aa827696db5df213
Brett Bash <brettbash@brettbash.com>
2025-01-21 11:51:48 -0600

    ✨ feat(yazi): plugins and theme

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,22 +1,46 @@
-require("zoxide"):setup({
-	update_db = true,
+require("full-border"):setup()
+
+require("githead"):setup()
+
+require("mactag"):setup({
+	-- Keys used to add or remove tags
+	keys = {
+		r = "Red",
+		o = "Orange",
+		y = "Yellow",
+		g = "Green",
+		b = "Blue",
+		p = "Purple",
+	},
+	-- Colors used to display tags
+	colors = {
+		Red = "#ee7b70",
+		Orange = "#f5bd5c",
+		Yellow = "#fbe764",
+		Green = "#91fc87",
+		Blue = "#5fa3f8",
+		Purple = "#cb88f8",
+	},
 })
+
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
 
 require("yatline"):setup({
 	section_separator = { open = "", close = "" },
 	part_separator = { open = "", close = "" },
-	inverse_separator = { open = "", close = "" },
+	-- inverse_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
 
 	style_a = {
 		fg = "black",
 		bg_mode = {
-			normal = "white",
+			normal = "blue",
 			select = "brightyellow",
-			un_set = "brightred",
+			un_set = "magenta",
 		},
 	},
-	style_b = { bg = "brightblack", fg = "brightwhite" },
-	style_c = { bg = "black", fg = "brightwhite" },
+	style_b = { bg = "#463465", fg = "#72f1b8" },
+	style_c = { bg = "#251c2e", fg = "#ff00ff" },
 
 	permissions_t_fg = "green",
 	permissions_r_fg = "yellow",
@@ -37,7 +61,7 @@ require("yatline"):setup({
 	found = { icon = "󰮕", fg = "blue" },
 	processed = { icon = "󰐍", fg = "green" },
 
-	show_background = true,
+	show_background = false,
 
 	display_header_line = true,
 	display_status_line = true,
@@ -54,10 +78,14 @@ require("yatline"):setup({
 		},
 		right = {
 			section_a = {
-				{ type = "string", custom = false, name = "date", params = { "%A, %d %B %Y" } },
+				{
+					type = "string",
+					custom = true,
+					name = "󱙧               󱙧 ",
+				},
 			},
 			section_b = {
-				{ type = "string", custom = false, name = "date", params = { "%X" } },
+				{ type = "coloreds", custom = false, name = "githead" },
 			},
 			section_c = {},
 		},
@@ -89,4 +117,48 @@ require("yatline"):setup({
 			},
 		},
 	},
+})
+
+require("yatline-githead"):setup({
+	show_branch = true,
+	branch_prefix = "",
+	branch_symbol = "",
+	branch_borders = "",
+
+	commit_symbol = " ",
+
+	show_behind_ahead = true,
+	behind_symbol = " ",
+	ahead_symbol = " ",
+
+	show_stashes = true,
+	stashes_symbol = " ",
+
+	show_state = true,
+	show_state_prefix = true,
+	state_symbol = "󱅉",
+
+	show_staged = true,
+	staged_symbol = " ",
+
+	show_unstaged = true,
+	unstaged_symbol = " ",
+
+	show_untracked = true,
+	untracked_symbol = " ",
+
+	prefix_color = "#ff00ff",
+	branch_color = "#e5fe5d",
+	commit_color = "#00ffff",
+	stashes_color = "#97e736",
+	state_color = "#abf3fd",
+	staged_color = "#72f1b8",
+	unstaged_color = "#fede5d",
+	untracked_color = "#ff00ff",
+	ahead_color = "#72f1b8",
+	behind_color = "#ff8b39",
+})
+
+require("zoxide"):setup({
+	update_db = true,
 })
