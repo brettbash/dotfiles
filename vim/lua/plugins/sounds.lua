17f4a9d1feea8873bc70ad86f9316b110f2cb8f9
Brett Bash <brettbash@Bretts-MBP.localdomain>
2025-02-13 13:25:08 -0600

    🚀 perf(sounds): using afplay and adjusted some sound usage

--- vim/lua/plugins/sounds.lua
+++ vim/lua/plugins/sounds.lua
@@ -1,23 +1,25 @@
 return {
   "EggbertFluffle/beepboop.nvim",
   opts = {
-    audio_player = "mpv",
+    audio_player = "afplay",
     max_sounds = 12,
     volume = 80,
     sound_map = {
       { trigger_name = "patdown", sound = "Pat Down.wav" },
       { trigger_name = "patup", sound = "Pat Up.wav" },
-      -- { auto_command = "VimEnter", sound = "Harpsichord.mp3" },
+      { trigger_name = "klink", sound = "Klink.wav" },
+      { auto_command = "VimEnter", sound = "Harpsichord.mp3" },
       -- { auto_command = "VimEnter", sound = "Fan Fare.mp3" },
       -- { auto_command = "VimLeave", sound = "AMFM.wav" },
       -- { auto_command = "VimLeave", sound = "open_flip1.oga" },
-      -- { auto_command = "BufRead", sound = "Knuckle.wav" },
+      -- { auto_command = "BufRead", sound = "Hi Tap.wav" },
       { auto_command = "BufNewFile", sound = "Dot.wav" },
       { auto_command = "BufDelete", sound = "Knuckle.wav" },
       { auto_command = "BufWritePost", sound = "Bam.wav" },
       { auto_command = "TextYankPost", sound = "Bloop.wav" },
-      { key_map = { mode = "n", key_chord = ":", blocking = false }, sound = "Schlik.wav" },
-      { auto_command = "TermOpen", sound = "Klink.wav" },
+      --      { key_map = { mode = "n", key_chord = ":", blocking = false }, sound = "Schlik.wav" },
+      -- { key_map = { mode = "n", key_chord = "  ", blocking = false }, sound = "Klink.wav" },
+      -- { auto_command = "TermOpen", sound = "Klink.wav" },
       -- 🧠 Additional Sound Ideas:
       ----- grug-far
       ----- Harpoon/Arrow
