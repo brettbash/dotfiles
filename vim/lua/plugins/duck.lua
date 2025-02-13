55008e449a229cd91d5da55a17b5e8caf0e11c15
Brett Bash <brettbash@Bretts-MBP.localdomain>
2025-02-13 13:15:20 -0600

    ✨ feat(duck): duck in vim now with more options

--- vim/lua/plugins/duck.lua
+++ vim/lua/plugins/duck.lua
@@ -1,11 +1,34 @@
 return {
   "tamton-aquib/duck.nvim",
   config = function()
-    vim.keymap.set("n", "<leader>dd", function()
+    vim.keymap.set("n", "<leader>D", "", { desc = "Duck 🦆" })
+
+    vim.keymap.set("n", "<leader>Dg", function()
       require("duck").hatch("👻", 10)
-    end, {})
-    vim.keymap.set("n", "<leader>dk", function()
+    end, { desc = "Spawn a Ghost 👻" })
+
+    vim.keymap.set("n", "<leader>Ds", function()
+      require("duck").hatch("🦑", 15)
+    end, { desc = "Summon the Kraken 🦑" })
+
+    vim.keymap.set("n", "<leader>Dc", function()
+      require("duck").hatch("🐙", 20)
+    end, { desc = "Summon the Cthulhu 🐙" })
+
+    vim.keymap.set("n", "<leader>Dd", function()
+      require("duck").hatch("🦆", 10)
+    end, { desc = "Hatch a Duck 🦆" })
+
+    vim.keymap.set("n", "<leader>Dd", function()
+      require("duck").hatch("🐉", 30)
+    end, { desc = "Call the Dragon 🐉" })
+
+    vim.keymap.set("n", "<leader>Dk", function()
       require("duck").cook()
-    end, {})
+    end, { desc = "Kill Kill Kill" })
+
+    vim.keymap.set("n", "<leader>Da", function()
+      require("duck").cook_all()
+    end, { desc = "Kill 'em All" })
   end,
 }
