1aa0146b072ececf0851280a367181159845f776
Brett Bash <brettbash@Bretts-MBP.localdomain>
2025-02-13 13:20:16 -0600

    ✨ feat(portal): vim portal for harpoon & lists

---
+++ vim/lua/plugins/portal.lua
@@ -0,0 +1,14 @@
+return {
+  "cbochs/portal.nvim",
+  dependencies = {
+    "cbochs/grapple.nvim",
+  },
+  config = function() end,
+
+  keys = {
+    { "<leader>P", "<cmd>lua require('portal.builtin').grapple.tunnel()<cr>", desc = "Harpoon Portal" },
+    { "<leader>J", "<cmd>lua require('portal.builtin').jumplist.tunnel()<cr>", desc = "Jumplist Portal" },
+    { "<leader>T", "<cmd>lua require('portal.builtin').changelist.tunnel()<cr>", desc = "Changelist Portal" },
+    { "<leader>F", "<cmd>lua require('portal.builtin').quickfix.tunnel()<cr>", desc = "Quickfix Portal" },
+  },
+}
