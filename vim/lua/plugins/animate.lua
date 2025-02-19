763722bb93e3ba0d5e44eb1b7ea1abcea01cb1ee
Brett Bash <brettbash@Bretts-MBP.localdomain>
2025-02-19 13:41:37 -0600

    ✨ feat(animation): refactored and added tiny glimmer

--- vim/lua/plugins/animate.lua
+++ vim/lua/plugins/animate.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "folke/snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+  },
+
+  {
     "echasnovski/mini.animate",
     opts = {
       cursor = { enable = false },
@@ -11,10 +18,70 @@ return {
       },
     },
   },
+
   {
     "sphamba/smear-cursor.nvim",
     opts = {
       legacy_computing_symbols_support = true,
     },
+  },
+
+  {
+    "rachartier/tiny-glimmer.nvim",
+    event = "TextYankPost",
+    keys = {
+      { "p" },
+      { "P" },
+      { "n" },
+      { "N" },
+      { "*" },
+      { "u" },
+      { "<C-r>" },
+      -- {
+      --   "n",
+      --   function()
+      --     require("tiny-glimmer").search_next()
+      --     vim.api.nvim_feedkeys("zzzv", "n", false)
+      --   end,
+      --   { noremap = true, silent = true },
+      -- },
+      -- {
+      --   "N",
+      --   function()
+      --     require("tiny-glimmer").search_prev()
+      --     vim.api.nvim_feedkeys("zzzv", "n", false)
+      --   end,
+      --   { noremap = true, silent = true },
+      -- },
+    },
+    opts = {
+      disable_warnings = false,
+      transparency_color = "#2a2139",
+      overwrite = {
+        -- default_animation = "rainbow",
+        search = { enabled = true },
+        paste = {
+          enabled = true,
+          paste_mapping = {
+            lhs = "p",
+            rhs = "<Plug>(YankyPutAfter)",
+          },
+          Paste_mapping = {
+            lhs = "P",
+            rhs = "<Plug>(YankyPutBefore)",
+          },
+        },
+        undo = { enabled = true },
+        redo = { enabled = true },
+      },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimAutocmdsDefaults",
+        callback = function()
+          vim.api.nvim_del_augroup_by_name("lazyvim_highlight_yank")
+        end,
+      })
+    end,
   },
 }
