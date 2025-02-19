ec673fd689cab5dc507fc181be2190edfe25e47e
Brett Bash <brettbash@Bretts-MBP.localdomain>
2025-02-19 13:46:38 -0600

    🐛 fix(yanky): turn highlighting off to work with tiny glimmer

---
+++ vim/lua/plugins/yanky.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "gbprod/yanky.nvim",
+    opts = {
+      keys = { { "p", false }, { "P", false } },
+      highlight = {
+        on_yank = false,
+        on_put = false,
+      },
+    },
+  },
+}
