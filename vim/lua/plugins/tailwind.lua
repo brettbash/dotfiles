c75b59a7b596134c1f673f3c8009415fdd303f40
Brett Bash <brettbash@Bretts-MBP.localdomain>
2025-02-24 15:32:16 -0600

    🗑️ remove(vim.plugins): removed plugins no longer needed

--- vim/lua/plugins/tailwind.lua
+++ vim/lua/plugins/tailwind.lua
@@ -27,26 +27,4 @@ return {
       },
     },
   },
-
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
-      { "tzachar/cmp-ai" },
-    },
-
-    opts = function(_, opts)
-      require("cmp").setup({
-        sources = {
-          { name = "cmp_ai" },
-        },
-      })
-      -- table.insert(opts.sources, 1, "cmp_ai")
-      local format_kinds = opts.formatting.format
-      opts.formatting.format = function(entry, item)
-        format_kinds(entry, item) -- add icons
-        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-      end
-    end,
-  },
 }
