197209c4f1403d2bd9a87b02a0cba71c03feca93
Brett Bash <brettbash@Bretts-MBP.localdomain>
2025-07-14 10:41:45 -0500

    🚀 perf(sounds): sound freeze came back, issue seemed to be the yank

--- vim/lua/plugins/sounds.lua
+++ vim/lua/plugins/sounds.lua
@@ -2,7 +2,7 @@ return {
   "EggbertFluffle/beepboop.nvim",
   opts = {
     audio_player = "afplay",
-    max_sounds = 4,
+    max_sounds = 2,
     -- volume = 100,
     sound_map = {
       { trigger_name = "patdown", sound = "Pat Down.wav" },
@@ -13,13 +13,13 @@ return {
       { trigger_name = "hitap", sound = "Hi Tap.wav" },
       { trigger_name = "dot", sound = "Dot.wav" },
 
-      -- { auto_command = "VimEnter", sound = "Harpsichord.mp3" },
+      { auto_command = "VimEnter", sound = "Harpsichord.mp3" },
       -- { auto_command = "VimLeavePre", sound = "AMFM.wav" },
       -- { auto_command = "BufNewFile", sound = "Dot.wav" },
       -- { auto_command = "BufWritePost", sound = "Tink.wav" },
       -- { auto_command = "TextYankPost", sound = "Bloop.wav" },
-      --
-      -- { key_map = { mode = "n", key_chord = ":", blocking = false }, sound = "Schlik.wav" },
+
+      { key_map = { mode = "n", key_chord = ":", blocking = false }, sound = "Schlik.wav" },
       -- { key_map = { mode = "n", key_chord = "  ", blocking = false }, sound = "Klink.wav" },
       -- { auto_command = "TermOpen", sound = "Klink.wav" },
 
