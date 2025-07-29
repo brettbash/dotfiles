68e27dcc5e51d4b366b99bc8fcd11c3625b2d65f
Brett Bash <brettbash@Bretts-MBP.localdomain>
2025-07-29 06:45:42 -0500

    ✨ feat(conform): made the default formatter to be pint

--- vim/lua/plugins/formatting.lua
+++ vim/lua/plugins/formatting.lua
@@ -61,6 +61,27 @@ return {
   },
 
   {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { "pint", "php_cs_fixer" },
+      },
+    },
+  },
+
+  -- Remove phpcs linter.
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        php = {},
+      },
+    },
+  },
+
+  {
     "NvChad/nvim-colorizer.lua",
     opts = {
       user_default_options = {
