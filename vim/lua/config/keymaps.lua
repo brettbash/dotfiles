773c47e068aa0667d0e0377ebee58d94aecb33c2
Brett Bash <brettbash@Bretts-MBP.localdomain>
2025-10-21 09:57:32 -0500

    🔈 sound: pulled back some sounds
    kept running into issues where freeze-ups would happen. turning these
    off seemed to have fixed the issue.

--- vim/lua/config/keymaps.lua
+++ vim/lua/config/keymaps.lua
@@ -3,32 +3,41 @@ vim.keymap.set({ "n", "v" }, "<F2>", "5j", { desc = "Down Fast" })
 vim.keymap.set({ "n", "v" }, "<F3>", "5k", { desc = "Up Fast" })
 vim.keymap.set({ "n", "v" }, "<F4>", "5w", { desc = "Word Forward Fast" })
 
-vim.keymap.set(
-  { "n", "v" },
-  "<tab>",
-  "<cmd>lua require('fzf-lua').buffers()<cr><cmd>lua require('beepboop').play_audio('klink')<cr>",
-  { desc = "Browse Buffers" }
-)
+-- vim.keymap.set(
+--   { "n", "v" },
+--   "<tab>",
+--   "<cmd>lua require('fzf-lua').buffers()<cr><cmd>lua require('beepboop').play_audio('klink')<cr>",
+--   { desc = "Browse Buffers" }
+-- )
+
+-- vim.keymap.set(
+--   { "n", "v" },
+--   "<leader><leader>",
+--   "<cmd>lua require('fzf-lua-enchanted-files').files()<cr><cmd>lua require('beepboop').play_audio('klink')<cr>",
+--   { desc = "Browse Files" }
+-- )
 
 vim.keymap.set(
   { "n", "v" },
   "<leader><leader>",
-  "<cmd>lua require('fzf-lua-enchanted-files').files()<cr><cmd>lua require('beepboop').play_audio('klink')<cr>",
+  "<cmd>lua require('fzf-lua-enchanted-files').files()<cr>",
   { desc = "Browse Files" }
 )
 
-vim.keymap.set(
-  { "n", "v" },
-  "<leader>bo",
-  "<cmd>lua require('beepboop').play_audio('knuckle')<cr><cmd>BufferLinePick<cr>",
-  { desc = "Pick Buffer" }
-)
-vim.keymap.set(
-  { "n", "v" },
-  "<leader>bc",
-  "<cmd>lua require('beepboop').play_audio('hollow')<cr><cmd>BufferLinePickClose<cr>",
-  { desc = "Pick Buffer to Close" }
-)
+vim.keymap.set({ "n", "v" }, "<leader>bo", "<cmd>BufferLinePick<cr>", { desc = "Pick Buffer" })
+
+-- vim.keymap.set(
+--   { "n", "v" },
+--   "<leader>bo",
+--   "<cmd>lua require('beepboop').play_audio('knuckle')<cr><cmd>BufferLinePick<cr>",
+--   { desc = "Pick Buffer" }
+-- )
+-- vim.keymap.set(
+--   { "n", "v" },
+--   "<leader>bc",
+--   "<cmd>lua require('beepboop').play_audio('hollow')<cr><cmd>BufferLinePickClose<cr>",
+--   { desc = "Pick Buffer to Close" }
+-- )
 
 vim.keymap.set({ "n", "v" }, "U", "<cmd>redo<cr>", { desc = "Redo" })
 vim.keymap.set("n", "<leader>on", "<CMD>Nvumi<CR>", { desc = "[O]pen [N]vumi" })
