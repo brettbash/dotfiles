1ae8e478710de1c28340af5f17d0a3a8256f90af
Brett Bash <brettbash@Bretts-MBP.localdomain>
2026-04-10 15:14:13 -0500

    🎨 ux(vim): power mode tweaks

--- vim/lua/plugins/power-mode.lua
+++ vim/lua/plugins/power-mode.lua
@@ -7,23 +7,51 @@ return {
       -- Cyberpunk neon color palette
       -- Each: { gui_fg, gui_bg, ctermfg, ctermbg }
       colors = {
-        color_1 = { "#00FFFF", default, 14, default }, -- Cyan
-        color_2 = { "#FF1493", default, 199, default }, -- Pink
-        color_3 = { "#BF00FF", default, 129, default }, -- Purple
-        color_4 = { "#39FF14", default, 46, default }, -- Green
-        color_5 = { "#FF6600", default, 202, default }, -- Orange
-        color_6 = { "#FFD700", default, 220, default }, -- Gold
-        color_7 = { "#00FF88", default, 48, default }, -- Teal
+        color_1 = { "#03edf9", default, 14, default }, -- Cyan
+        color_2 = { "#ff7edb", default, 199, default }, -- Pink
+        color_3 = { "#5f3fff", default, 129, default }, -- Purple
+        color_4 = { "#7ee787", default, 46, default }, -- Green
+        color_5 = { "#ff8b39", default, 202, default }, -- Orange
+        color_6 = { "#fede5d", default, 220, default }, -- Gold
+        color_7 = { "#88d1ff", default, 48, default }, -- Teal
         color_8 = { "#FF00FF", default, 201, default }, -- Magenta
       },
       -- fire_wall = { enabled = true },
+      combo = {
+        enabled = true, -- Show combo counter
+        position = "top-right", -- "top-right"|"top-left"|"bottom-right"|"bottom-left"
+        width = 20, -- Window width
+        height = 6, -- Window height
+        timeout = 3000, -- ms before combo resets
+        thresholds = { 10, 25, 50, 100, 200 }, -- Level escalation thresholds
+        shake = true, -- Shake combo window on keystroke
+        shake_intensity = nil, -- Override: { min, max } (nil = auto)
+        exclamations = { -- Random phrases at milestones
+          "UNSTOPPABLE!",
+          "GODLIKE!",
+          "RAMPAGE!",
+          "MEGA KILL!",
+          "DOMINATING!",
+          "WICKED SICK!",
+          "LEGENDARY!",
+        },
+        exclamation_interval = 10, -- Show phrase every N keystrokes
+        exclamation_duration = 1500, -- ms to display phrase
+        level_colors = { -- Colors per level: { gui_fg, ctermfg }
+          [0] = { "#97e736", default }, -- Green
+          [1] = { "#fede5d", 14 }, -- Cyan
+          [2] = { "#ff7edb", 199 }, -- Pink
+          [3] = { "#ff00ff", 129 }, -- Purple
+          [4] = { "#fe4450", 196 }, -- Red
+        },
+      },
     })
   end,
 
   keys = {
     {
       "<leader>pp",
-      "<cmd>PowerModeToggle<cr><cmd>lua require('beepboop').play_audio('knuckle')<cr><cmd>lua require('beepboop').play_audio('toasty')<cr>",
+      "<cmd>PowerModeToggle<cr><cmd>lua require('beepboop').play_audio('knuckle')<cr><cmd>lua require('beepboop').play_audio('starfox')<cr>",
       desc = "Power Mode Toggle ⚡",
     },
   },
