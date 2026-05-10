3241610f77e411eb4eaccb0efb7546c9929e2528
Brett Bash <brettbash@Bretts-MBP.localdomain>
2026-05-10 05:39:40 -0500

    ✨️ feat(sidekick): added a crush specific key binding

--- vim/lua/plugins/ai.lua
+++ vim/lua/plugins/ai.lua
@@ -1,1 +1,21 @@
-return {}
+return {
+  {
+    "folke/sidekick.nvim",
+    opts = {
+      cli = {
+        tools = {
+          crush = {},
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>aA",
+        function()
+          require("sidekick.cli").toggle({ name = "crush" })
+        end,
+        desc = "Sidekick (Crush)",
+      },
+    },
+  },
+}
